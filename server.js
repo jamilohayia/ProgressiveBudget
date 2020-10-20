229cf55c7a453ef57e8eb7d41ae2f110001b8954
Jamil Ohayia <jamil.ohayia@columbia.edu>
2020-10-20 01:11:19 -0400

    added the mongoose connection

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/progressive-budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
